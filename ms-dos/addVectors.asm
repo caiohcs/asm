9c491a1a272e15d4befc2c345646d382bbbe0802
Caio Henrique Costa Souza <caio.lief@gmail.com>
2020-07-02 15:09:13 -0300

    refactored addvectors

--- ms-dos/addVectors.asm
+++ ms-dos/addVectors.asm
@@ -1,49 +1,13 @@
 ;;;*********************************************************************
-;;; 			Adds two vectors of three elements
+;;; 			Adds two vectors of three positive elements,
+;;; 			  each element can have up to 3 digits.
+;;; 			Each element is inserted via keyboard.
 ;;;*********************************************************************
 	
 	DOSSEG
 	.MODEL	SMALL
-	.STACK					; Empty stack.
-	.RADIX	16				; Using HEX base.
-	
-;;;******************************* MACROS *******************************
-
-Breakline MACRO
-	MOV	DL, 0AH
-	MOV	AH, 02H
-	INT	21H
-	MOV	DL, 0DH
-	MOV	AH, 02H
-	INT	21H
-ENDM
-
-ReadStrFromKbd MACRO strOffset
-	MOV	DX, OFFSET strOffset		; Reads a string and stores the string
-	MOV	AH, 0AH				;  in strOffset.
-	INT	21H
-	Breakline
-ENDM
-
-PrintStr MACRO msg
-	MOV	DX, OFFSET msg			; Prints a string.
-	MOV	AH, 09H
-	INT	21H
-	Breakline
-ENDM
-
-String2Number MACRO num, str
-	MOV	DS:[tmpaddr], OFFSET num 	; Converts the string str to a number
-	MOV	BP, OFFSET str			;  stored at num
-	ADD	BP, 02H
-	CALL	Str2Num
-ENDM
-	
-Number2String MACRO str, num
-	MOV	BP, OFFSET num			; Converts a number stored at vecRES[0] to
-	MOV	DS:[tmpaddr], OFFSET str	;  a string stored at strRES1.
-	CALL	Num2Str
-ENDM
+	.STACK
+	.RADIX	10				; Using base 10.
 
 ;;;********************************* DATA ********************************
 	
@@ -51,57 +15,98 @@ ENDM
 	msg1	DB 	"Insert three numbers for the first vector:$"
 	msg2	DB 	"Insert three numbers for the second vector:$"
 	msg3	DB 	"The sum of the two vectors is:$"
-	strA1	DB 	06H,00H,6 dup (00H)
-	strA2	DB 	06H,00H,6 dup (00H)
-	strA3	DB 	06H,00H,6 dup (00H)
-	strB1	DB 	06H,00H,6 dup (00H)
-	strB2	DB 	06H,00H,6 dup (00H)
-	strB3	DB 	06H,00H,6 dup (00H)
-	strRES1	DB 	5 dup (00H),'$'
-	strRES2	DB 	5 dup (00H),'$'
-	strRES3	DB 	5 dup (00H),'$'
-	tmpaddr	DW	0000H
-	vecA	DW	3 dup (0000H)
-	vecB	DW	3 dup (0000H)
-	vecRES	DW	3 dup (0000H)
-	ten	DW	0AH
-	res	DW	00H
-	tmpmul	DW	01H
+	msgCR	DB	0AH, 0DH, '$' 		; Used to break line.
+	strA1	DB 	06H,00H,6 dup (00H)	; Will store the element A[0] as an ASCII string.
+	strA2	DB 	06H,00H,6 dup (00H)	; Will store the element A[1] as an ASCII string.
+	strA3	DB 	06H,00H,6 dup (00H)	; Will store the element A[2] as an ASCII string.
+	strB1	DB 	06H,00H,6 dup (00H) 	; Will store the element B[0] as an ASCII string.
+	strB2	DB 	06H,00H,6 dup (00H) 	; Will store the element B[1] as an ASCII string.
+	strB3	DB 	06H,00H,6 dup (00H) 	; Will store the element B[2] as an ASCII string.
+	strRES1	DB 	5 dup (00H),'$'		; Will store the element RES[0] as an ASCII string.
+	strRES2	DB 	5 dup (00H),'$'		; Will store the element RES[1] as an ASCII string.
+	strRES3	DB 	5 dup (00H),'$'		; Will store the element RES[2] as an ASCII string.
+	tmpaddr	DW	0000H			; Temporary variable for addresses.
+	vecA	DW	3 dup (0000H)		; Will store A[0] - A[2] in binary form.
+	vecB	DW	3 dup (0000H)		; Will store A[0] - A[2] in binary form.
+	vecRES	DW	3 dup (0000H)		; Will store RES[0] - RES[2] in binary form.
+	ten	DW	000AH			; Constant 10.
+	res	DW	0000H			; Temporary value for results.
+	tmpmul	DW	0001H			; Temporary value for multiplications on string 2 bin conversions.
+ 	
+;;;******************************* MACROS *******************************
+
+Breakline MACRO					
+	MOV	DX, OFFSET msgCR		; Break line (i.e. <ENTER>)
+	MOV	AH, 09H
+	INT	21H
+ENDM
+
 	
+ReadStrFromKbd MACRO strOffset
+	MOV	DX, OFFSET strOffset		; Reads a string from keyboard and stores the string
+	MOV	AH, 0AH				;  at DS:[strOffset].
+	INT	21H
+	Breakline
+ENDM
+
+	
+PrintStr MACRO msg
+	MOV	DX, OFFSET msg			; Prints a string located at DS:[msg], ended by the char '$'.
+	MOV	AH, 09H
+	INT	21H
+	Breakline
+ENDM
+
+	
+String2Number MACRO num, str
+	MOV	DS:[tmpaddr], OFFSET num	; Puts the arguments in the appropriate locations before calling
+	MOV	BX, OFFSET str			;  the procedure Str2Num.
+	ADD	BX, 01H
+	CALL	Str2Num				; Calls the procedure that converts an ASCII string to binary.
+ENDM						; The first element of the string must be the number of characters.
+
+	
+Number2String MACRO str, num
+	MOV	BX, OFFSET num			; Puts the arguments in the appropriate locations before calling
+	MOV	DS:[tmpaddr], OFFSET str	;  the procedure Num2Str.
+	CALL	Num2Str				; Calls the procedure that converts a binary number
+ENDM						;  to an ASCII string.
+
 ;;;********************************** CODE *******************************
 	
 	.CODE
 
 ;;;******************************* Str2Num *******************************
-;;;	Receives a string starting with offset stored in BP ended by 0DH (CR).
+;;;	Receives a string starting with offset stored in BX ended by 0DH (CR).
 ;;;	Stores the result at offset stored in tmpaddr.
+;;;	The first element of the string must be the number of characters.
 Str2Num	PROC
-;;;		 Calculating the size of the string:
-	XOR	SI, SI
-CalcStrSize:	
-	CMP	BYTE PTR DS:[BP + SI], 0DH
-	JE	EndCalcStrSize
-	INC	SI
-	JMP	CalcStrSize
-EndCalcStrSize:					; Now SI is holding the num of chars in the string.
-	MOV	CX, SI
-;;; 		Starts converting the string to a number:
+	XOR	CH, CH
+	MOV	CL, DS:[BX]			; Puts the number of chars in CX.
+	INC	BX				; Now BX points to the first character.
+	MOV	SI, CX				; Puts the number of chars in SI.
+	DEC	SI				; Now SI points to the last char.
 	MOV	DX, 0001H
-	MOV	DS:[tmpmul], DX
+	MOV	DS:[tmpmul], DX			; Initialize the variable tmpmul with 1.
+	MOV	AX, DX				; Initializes AX with 1.
 	XOR	DX, DX
-	MOV	DS:[res], DX
-	DEC	SI
+	MOV	DS:[res], DX			; Initialize the variable res with 0.
+	
+						; The following loop iterates the string in reverse order
+						;  converting each ASCII char to binary then
+						;  it multiplies the corresponding digit by 1, 10, 100, 1000 etc. 
+						; The variable res will accumulate the sum of the digits.
+						; The variable tmpmul will hold the powers of 10: 1, 10, 100 etc.
 ConvStr2Num:
-	XOR	BX, BX
-	MOV	BL, DS:[BP + SI]
-	SUB	BL, '0'
-	DEC	SI
-	MOV	AX, DS:[tmpmul]
-	MUL	BX
+	XOR	DX, DX
+	MOV	DL, DS:[BX + SI]
+	SUB	DL, '0'
+	MUL	DX
 	ADD	DS:[res], AX
 	MOV	AX, DS:[tmpmul]
 	MUL	DS:[ten]
 	MOV	DS:[tmpmul], AX
+	DEC	SI
 	LOOP	ConvStr2Num
 ENDstr2Num:
 	MOV	SI, DS:[tmpaddr]
@@ -111,18 +116,24 @@ ENDstr2Num:
 Str2Num	ENDP
 
 ;;;******************************* Num2Str *******************************
-;;;	Receives a 16-bits number with offset stored in BP.
-;;;	Stores the result at offset stored in tmpaddr.
+;;;	Receives a 16-bits number with offset stored in BX, converts it to
+;;; 	 an ASCII string representation, then stores the string at offset
+;;;	 stored in tmpaddr.
 Num2Str	PROC
 	XOR	SI, SI
 	XOR	CX, CX
-	MOV	AX, DS:[BP]
-	MOV	BP, DS:[tmpaddr]
-	MOV	BL, 0AH
+	MOV	AX, DS:[BX]			; Puts the number in AX.
+	MOV	BX, DS:[tmpaddr]		; BX now points to the first char of the string.
+	MOV	DL, 0AH				; Puts 10 in DL.
+
+						; The following loop divides the number by 10 at each interation
+						;  to get each digit. Since we get the digits at reverse order,
+						;  we push them in the stack, so that we can push them latter
+						;  to fix the order of the digits.
 ConvNum2Str:
 	CMP	AX, 0000H
 	JE	ENDConvNum2str
-	IDIV	BL
+	IDIV	DL
 	PUSH	AX
 	XOR	AH, AH
 	INC	SI
@@ -130,29 +141,29 @@ ConvNum2Str:
 ENDConvNum2str:
 	MOV	CX, SI
 	XOR	SI, SI
-ReversNum2str:
+ReversNum2str:					; Retrieves the digits from the stack and coverts them to ASCII.
 	POP	AX
 	ADD	AH, '0'
-	MOV	DS:[BP + SI], AH
+	MOV	DS:[BX + SI], AH
 	INC	SI
 	LOOP	ReversNum2str
 	RET
-Num2Str	ENDP	
+Num2Str	ENDP
 ;;;******************************* Main *******************************
 	
 Main	PROC
 	MOV	AX, @DATA			; Copy data segment addr to DS.
 	MOV	DS, AX
 	
-	PrintStr msg1				; Prints msg1.
+	PrintStr msg1
 
-	ReadStrFromKbd strA1			; Reads strings from keyboard.
+	ReadStrFromKbd strA1
 	ReadStrFromKbd strA2
 	ReadStrFromKbd strA3
 
-	PrintStr msg2				; Prints msg2.
+	PrintStr msg2
 
-	ReadStrFromKbd strB1			; Reads strings from keyboard.
+	ReadStrFromKbd strB1
 	ReadStrFromKbd strB2
 	ReadStrFromKbd strB3
 	
@@ -166,13 +177,13 @@ Main	PROC
 
 	MOV	SI, 0000H
 	MOV	CX, 0003H
-AddVecs:
-	MOV	BP, offset vecA
-	MOV	AX, DS:[BP + SI]
-	MOV	BP, offset vecB
-	ADD	AX, DS:[BP + SI]
-	MOV	BP, offset vecRES
-	MOV	DS:[BP + SI], AX
+AddVecs:					; Loop to add the two vectors
+	MOV	BX, offset vecA
+	MOV	AX, DS:[BX + SI]
+	MOV	BX, offset vecB
+	ADD	AX, DS:[BX + SI]
+	MOV	BX, offset vecRES
+	MOV	DS:[BX + SI], AX
 	ADD	SI, 0002H
 	LOOP	AddVecs
 
@@ -180,11 +191,11 @@ AddVecs:
 	Number2String strRES2, vecRES+02H
 	Number2String strRES3, vecRES+04H
 	
-	PrintStr msg3				; Prints msg3.
+	PrintStr msg3
 	
-	PrintStr strRES1			; Prints strRES1.
-	PrintStr strRES2			; Prints strRES2.
-	PrintStr strRES3			; Prints strRES3.
+	PrintStr strRES1
+	PrintStr strRES2
+	PrintStr strRES3
 	
 	MOV	AH, 4CH				; Calls the end of program DOS
 	INT	21H				;  interruption routine.
